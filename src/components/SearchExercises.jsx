909e12046222964dda5e57e228daab5f51ead0d9
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-02-13 23:10:34 +0500

    add: fetch func to get data from rapid api exercises

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,13 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 export const SearchExercises = () => {
+
+  const [search, setSearch] = useState('');
+  
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      console.log(exercisesData);
+    }
+  }
+
+
   return (
     <Stack alignItems='center' justifyContent='center' mt='37px' p='20px'>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} textAlign='center' mb='50px'>
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} textAlign='center' mb='50px' mt='40px'>
         Awesome Exercises You <br />
         Should Know
       </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: "#FFF",
+            borderRadius: '40px',
+          }}
+          height='76px'
+          value={search}
+          onChange={(e) => { setSearch(e.target.value.toLowerCase()) }}
+          placeholder='Search Exercises'
+          type='text'
+        />
+        <Button className='search-btn'
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#FFF',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '12px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+          onClick={handleSearch}>
+          Search</Button>
+      </Box>
+
     </Stack>
   )
 }
---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,17 @@
+const apiKey = import.meta.env.VITE_RAPID_API_KEY;
+
+export const exerciseOptions = {
+  method: 'GET',
+  params: {limit: '10'},
+  headers: {
+    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
