75e6b1c70ad2d8446ee8e1b87fc45635601b3b86
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-02-13 21:43:23 +0500

    updt: small fixes to package.json

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -4,7 +4,7 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 export const SearchExercises = () => {
   return (
     <Stack alignItems='center' justifyContent='center' mt='37px' p='20px'>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }}>
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} textAlign='center' mb='50px'>
         Awesome Exercises You <br />
         Should Know
       </Typography>
